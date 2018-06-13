4ed52c20cde52d389c9728a504c6b14a6eec292a
Parker Hall <parkerh@mit.edu>
2018-06-12 22:19:25 -0400

    added chop and quick exit functionality

--- tracker.py
+++ tracker.py
@@ -1,7 +1,7 @@
 # TRACKS REAL-LIFE POKER GAME BEING PLAYED
 
 NUM_BETTING_ROUNDS = 4
-ACTIONS = set(["check", "raise", "call", "fold"])
+ACTIONS = set(["check", "raise", "call", "fold", "chop"])
 
 ROUND_DICT = {0: "PRE-FLOP", 1: "POST-FLOP", 2: "POST-TURN", 3: "POST-RIVER"}
 
@@ -87,15 +87,17 @@ def play_hand(player_list, player_dict, button, small_blind, big_blind):
 	for i in range(NUM_BETTING_ROUNDS):
 		print(ROUND_DICT[i])
 		new_player_list, new_pot = betting_round(current_player_list, player_dict, button, bet_dict, offset)
-		print(new_player_list)
-		pot += new_pot
 
 		if new_player_list == None and new_pot == None:
+			player_dict[sb] += small_blind
+			player_dict[bb] += big_blind
 			return False
 		elif len(new_player_list) == 1:
 			winner = new_player_list[0]
 			player_dict[winner] += pot
 			return True
+
+		pot += new_pot
 
 		if i == 0:
 			current_player_list = []
@@ -140,21 +142,24 @@ def betting_round(player_list, player_dict, button, bet_dict, offset):
 	can_check = True
 
 	have_played = 0
+	num_folded = 0
 
 	while True:
 		for i in range(len(player_list)):
 			current_player = player_list[(start_index + i) % num_players]
 			if bet_dict[current_player] != "FOLD":
-				fold_status = [bet_dict[player] == "FOLD" for player in player_list if player != current_player]
-				if all(fold_status):
+				if num_players - num_folded == 1:
 					return [current_player], new_pot
 
 				print_bets(bet_dict)
-				action, bad_action = player_action(current_player, can_check)
+				action, bad_action = player_action(current_player, player_dict, bet_dict, can_check, max_bet)
+
+				if action == None:
+					return action, action
 
 				while bad_action:
 					print("Please enter 'check', 'call', 'fold' or 'raise NUM'")
-					action, bad_action = player_action(current_player, can_check)
+					action, bad_action = player_action(current_player, player_dict, bet_dict, can_check, max_bet)
 
 				add_to_pot, add_to_max = handle_action(current_player, action, player_dict, bet_dict, max_bet)
 				new_pot += add_to_pot
@@ -163,8 +168,17 @@ def betting_round(player_list, player_dict, button, bet_dict, offset):
 				keyword = action[0].lower()
 				if keyword == "fold":
 					bet_dict[current_player] = "FOLD"
-				if keyword == "raise":
+					num_folded += 1
+				elif keyword == "raise":
 					can_check = False
+				elif keyword == "chop":
+					current_bet = bet_dict[current_player]
+					
+					player_dict[current_player] += current_bet
+					new_pot -= current_bet
+					bet_dict[current_player] = "FOLD"
+					winner = [player for player in bet_dict if bet_dict[player] != "FOLD"]
+					return winner, new_pot
 
 				have_played += 1
 
@@ -187,13 +201,51 @@ def betting_round(player_list, player_dict, button, bet_dict, offset):
 		start_index = 0
 
 
-def player_action(current_player, can_check):
+def player_action(current_player, player_dict, bet_dict, can_check, max_bet):
 	action = raw_input("Action " + current_player.upper() + ": ").split(' ')
 	keyword = action[0].lower()
+	if keyword == "":
+		return None, None
 	bad_keyword = keyword not in ACTIONS
 	bad_check = ((keyword == "check") and (not can_check))
-	bad_action = bad_keyword or bad_check
+	if keyword == "raise":
+		not_allowed = check_raise(current_player, player_dict, bet_dict, max_bet, action)
+	elif keyword == "call":
+		not_allowed = check_call(current_player, player_dict, bet_dict, max_bet)
+	elif keyword == "chop":
+		not_allowed = check_chop(current_player, bet_dict)
+	else:
+		not_allowed = False
+	bad_action = bad_keyword or bad_check or not_allowed
 	return action, bad_action
+
+def check_raise(current_player, player_dict, bet_dict, max_bet, action):
+	try:
+		extra = int(action[1])
+	except:
+		print("Please enter an integer NUM for 'raise NUM'")
+		return True
+	if extra < 0:
+		print("Please enter a positive integer NUM for 'raise NUM'")
+		return True
+	needed = max_bet + extra - bet_dict[current_player]
+	bad_raise = player_dict[current_player] < needed
+	if bad_raise:
+		print("Not enough funds")
+	return bad_raise
+
+def check_call(current_player, player_dict, bet_dict, max_bet):
+	bad_call = player_dict[current_player] < (max_bet - bet_dict[current_player])
+	if bad_call:
+		print("Not enough funds")
+	return bad_call
+
+def check_chop(current_player, bet_dict):
+	players_remaining = sum([bet_dict[player] != "FOLD" for player in bet_dict])
+	bad_chop = players_remaining != 2
+	if bad_chop:
+		print("Can only chop with two players remaining")
+	return bad_chop
 
 def handle_action(current_player, action, player_dict, bet_dict, max_bet):
 	keyword = action[0].lower()
